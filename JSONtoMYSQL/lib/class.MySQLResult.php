891cd0dc50bb59a608f564bf0059ff40d9966d6e
Adam Wulf <adam.wulf@gmail.com>
2018-10-08 16:55:57 -0400

    Can peek fetching a row from a result

--- JSONtoMYSQL/lib/class.MySQLResult.php
+++ JSONtoMYSQL/lib/class.MySQLResult.php
@@ -8,12 +8,14 @@ class MySQLResult{
 	private $result;
 	private $insert_id;
 	private $affected_rows;
+	private $offset;
 
 
 	public function __construct($link, $result){
 		$this->result = $result;
 		$this->insert_id = @mysqli_insert_id($link);
 		$this->affected_rows = @mysqli_affected_rows($link);
+		$this->offset = 0;
 	}
 
 	
@@ -22,7 +24,24 @@ class MySQLResult{
 	}
 
 	function fetch_array(){
-		return mysqli_fetch_array($this->result, MYSQLI_ASSOC);
+		if($this->offset < $this->num_rows()){
+			$this->offset += 1;
+			return mysqli_fetch_array($this->result, MYSQLI_ASSOC);
+		}
+		return null;
+	}
+	
+	function peek_array(){
+		$offset = $this->offset;
+		$ret = $this->fetch_array();
+		
+		if($this->offset != $offset){
+			// restore pointer to previous record
+			mysqli_data_seek($this->result, $offset);
+			$this->offset = $offset;
+		}
+		
+		return $ret;
 	}
 	
 	function insert_id(){
@@ -32,9 +51,15 @@ class MySQLResult{
 	function affected_rows(){
 		return $this->affected_rows;
 	}
+	
+	function has_next(){
+		return mysqli_more_results($this->result);
+	}
 
-	function rewide(){
-		return mysqli_data_seek($this->result, 0);
+	function rewind(){
+		if($this->num_rows() > 0){
+			return mysqli_data_seek($this->result, 0);
+		}
 	}
 }
 
